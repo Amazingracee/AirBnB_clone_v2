e421c3f55e1aa3e03155f21be6f625a8b4ddd2c1
Grace <olabodephoeb@gmail.com>
2023-01-20 04:08:16 +0000

    pycodestyle error

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" 
-This is a script that starts a Flask web application
+"""
+Starts a simple web application listening on 0.0.0.0:5000
 """
 
-from flask import FLASK
+from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
